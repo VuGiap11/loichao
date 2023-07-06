af4a90fe627509c7531ab25c024c7eebb32d719e
vu giap <vudinhgiap11@gmail.com>
2023-07-06 10:33:16 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loichao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Xin chao: "+yourName);
+        }
+    }
+}
